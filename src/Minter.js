06489fc565f5d5f9c49f558d8a90f02d35670a47
Oluka Isaac <hilghsilk2damson@gmail.com>
2022-11-19 18:31:35 +0100

    updated project

---
+++ src/Minter.js
@@ -0,0 +1,200 @@
+import { useEffect, useReducer } from "react";
+import { connectWallet, mintNFT, getCurrentwalletConnected } from "./utils/interact";
+
+
+const nftMintReducer = (state, action) => {
+  switch (action.type) {
+    case "CONNECT_WALLET": 
+      return {
+        ...state,
+        status: action.status,
+        walletAddress: action.walletAddress
+      }
+    case "TEXT_INPUT": 
+      return {
+        ...state,
+        [action.payload.key]: action.payload.value,
+      }
+    case "MINTING":
+      return {
+        ...state,
+        minting: true,
+      }
+    case "MINT" : 
+      if(action.payload === "success" || action.payload === "error"){
+        return{
+          ...state,
+          minting:false,
+          status:action.status
+        }
+      }
+      return {
+        ...state,
+        minting: false,
+        status: action.status
+      }
+    case "ACCOUNT_CHANGES" :
+      if(action.payload.length > 0 ){
+        return{
+          ...state,
+          walletAddress: action.payload[0],
+          status: "👆🏽 Write a message in the text-field above."
+        }
+      }
+      return{
+        ...state,
+        walletAddress: "",
+        status: "🦊 Connect to Metamask using the top right button."
+
+      }
+    case "WALLET_LISTENER_ONERROR" : 
+      return{
+        ...state,
+        status: <p>
+        {" "}
+        🦊{" "}
+        <a target="_blank" href={`https://metamask.io/download.html`}>
+          You must install Metamask, a virtual Ethereum wallet, in your
+          browser.
+        </a>
+      </p>
+      }
+    default:
+    break;
+  }
+}
+
+
+const initialState = {
+  walletAddress: "",
+  status: "",
+  name: "",
+  description: "",
+  url: "",
+  minting: false
+}
+
+const Minter = (props) => {
+  const [state, dispatch] = useReducer(nftMintReducer, initialState);
+  console.log(state)
+  //State variables
+  const {name, description, url, walletAddress, status, minting} = state;
+  console.log(window);
+ 
+  useEffect(async () => {
+    const {address, status} = await getCurrentwalletConnected();
+    dispatch({
+      type: 'CONNECT_WALLET', 
+      status,
+      walletAddress: address
+    })
+    addWalletListener(dispatch);
+  }, []);
+
+  
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    dispatch({
+      type: 'CONNECT_WALLET', 
+      status:walletResponse.status,
+      walletAddress: walletResponse.address
+    })
+  };
+
+  const onMintPressed = async () => {
+    dispatch({type: 'MINTING'});
+    const {statusMessage, status} = await mintNFT(url, name, description);
+    dispatch({type: 'MINT', status:statusMessage, payload: status});
+    // console.log(statusMessage,status);
+    //console.log(url, name, description);
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          <div className="connected">
+            <span>Connected: </span>
+            <span className="address">{String(walletAddress).substring(0, 4) +
+            "..." +
+            String(walletAddress).substring(38)}</span>
+          </div>
+        ) :(
+          <span>Connect Wallet</span>
+        )}
+      </button>
+       <div className="header">
+          <h2 className="title"> My NFT Minter</h2>
+          <p className="form_details">
+            Simply add your asset's link, name, and description, then press "Mint."
+          </p>
+       </div>
+      <form>
+        <h3>Link to asset: </h3>
+        <input
+          type="text"
+          placeholder="Link to Asset..."
+          onChange={(event) => 
+            dispatch({
+              type: "TEXT_INPUT",
+              payload: {
+                key: "url", value: event.target.value
+              },
+            })
+          }
+        />
+        <h3>Name: </h3>
+        <input
+          type="text"
+          placeholder="Enter NFT Name..."
+          onChange={(event) => 
+            dispatch({
+              type: "TEXT_INPUT",
+              payload: {
+                key: "name", value: event.target.value
+              },
+            })
+          }
+        />
+        <h3>Description: </h3>
+        <input
+          type="text"
+          placeholder="Enter NFT Description..."
+          onChange={(event) => 
+            dispatch({
+              type: "TEXT_INPUT",
+              payload: {
+                key: "description", value: event.target.value
+              },
+            })
+          }
+        />
+      </form>
+       <div className="button_div">
+        <button id="mintButton" onClick={onMintPressed}>
+          {minting? "Minting..." : "Mint NFT"}
+        </button>
+       </div>
+      <div
+       id="status">
+        {status}
+      </div>
+    </div>
+  );
+
+  function addWalletListener(dispatch){
+    if(window.ethereum) {
+      window.ethereum.on("accountsChanged",  (accounts) => {
+        console.log("acount: ", accounts)
+        dispatch({type: "ACCOUNT_CHANGES", payload:accounts})
+      });
+    } else{
+      dispatch({type: "WALLET_LISTENER_ONERROR"})
+    }
+  }
+  
+};
+
+
+
+export default Minter;
---
+++ src/utils/interact.js
@@ -0,0 +1,179 @@
+import  { pinJSONToIPFS } from './pinata.js';
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const  { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+require('dotenv').config();
+
+const contractABI = require('../contract-abi.json');
+const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
+
+//Connnect Dapp to Ethereum account(Metamask);
+export const connectWallet = async () => {
+  if(window.ethereum) {
+    try{
+      const addressArray = await window.ethereum.request({
+      method: "eth_requestAccounts",
+      });
+      console.log(addressArray)
+      const obj = {
+        status: "👆🏽 Write a message in the text-field above.",
+        address: addressArray[0],
+      };
+      console.log(obj)
+      return obj;
+    } 
+    catch (err) {
+      return {
+        address: "",
+        status: "😥" + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {""}
+            🦊{" "}
+            <a 
+            rel='noreferrer'
+            target= "_blank" href = {`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your browser.
+            </a>
+          </p>
+        </span>
+      )
+    }
+  }
+}
+
+
+//Mint NFT
+export const mintNFT = async (url, name, description) => {
+  //error handling
+  if (url.trim() === "" || (name.trim() === "" || description.trim() === "")) {
+    return{
+      success: false,
+      statusMessage: "!Please make sure all fields are completed before minting."
+    }
+  }
+
+  //make metadata
+  const metadata = new Object();
+  metadata.name = name;
+  metadata.image = url;
+  metadata.description = description;
+
+  //make pinata call
+  const pinataResponse = await pinJSONToIPFS(metadata);
+
+  //console.log(pinataResponse)
+  if(!pinataResponse.success) {
+    return {
+      success: false,
+      statusMessage: "😢 Something went wrong while uploading your tokenURI.",
+    }
+  }
+  const tokenURI = pinataResponse.pinataUrl;
+
+  //load smart contract
+  //window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+  window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+  console.log(window)
+
+  //set up your Ethereum transaction
+  const transactionParameters = {
+    to: contractAddress, //Required except during contract publications.
+    from: window.ethereum.selectedAddress, //must match user's active address.
+    'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI() //make call to NFT smart contract
+  };
+
+  //sign the transaction via Metamask
+  try{
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+    return  {
+      success: "true",
+      statusMessage:
+      (
+        <span>
+          <div style={{width: '60%', textAlign: "center", color:'#4FBF26'}}>
+          ✅ Check out your transaction on Etherscan:
+            <a target="_blank"
+            rel='noreferrer'
+             href={'https://ropsten.etherscan.io/tx/' + txHash}>
+             Here
+            </a>
+          </div>
+        </span>
+      ),
+      status: "success"
+    }
+
+  }
+  catch (error) {
+    return{
+      success: false,
+      statusMessage: "😥 Something went wrong:" + error.message,
+      status: "error"
+    }
+  }
+  // finally{
+  //   return{
+  //     status: "finally"
+  //   }
+  // }
+}
+
+
+
+
+
+//Get Wallet Details  in case of refresh.
+export const getCurrentwalletConnected = async () => {
+  if(window.ethereum) {
+    try{
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if(addressArray.length > 0) {
+        return{
+          address: addressArray[0],
+          status: "👆🏽 Write a message in the text-field above.",
+        };
+      } else {
+        return {
+          address: "",
+          status: "🦊 Connect to Metamask using the top right button."
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😥 " + err.message,
+      };
+    }
+  }else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a target="_blank"
+            rel='noreferrer'
+             href={`https://metamask.io/download.html`}>
+              You must install Metamask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+}
